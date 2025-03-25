cb96580c52e543877c252e60b7ee07882b308feb
jectatham@gmail.com <jectatham@gmail.com>
2025-03-24 20:06:11 -0400

    fix typehandling

--- src/Components/ErrorMessage.tsx
+++ src/Components/ErrorMessage.tsx
@@ -4,14 +4,14 @@ import { Alert } from 'react-bootstrap';
 export interface ErrorProps {
   errorMessage?: string,
 }
-const ErrorMessage = ({ errorMessage }: { errorMessage: ErrorProps }) => {
+const ErrorMessage: React.FC<ErrorProps> = ({ errorMessage }) => {
   console.log(errorMessage)
   if (!errorMessage) {
     return;
   }
   return (
     <Alert key="add-book-error" variant="danger">
-      {/* {errorMessage} */}
+      <p>{errorMessage}</p>
     </Alert>
   )
 }
